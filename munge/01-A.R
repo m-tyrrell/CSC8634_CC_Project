011e77f10b8a4d350754313a3c864a48283ab483
m-tyrrell <mtyrrell@ymail.com>
2019-01-14 12:38:33 +0000

    Q1/q2

--- munge/01-A.R
+++ munge/01-A.R
@@ -6,11 +6,11 @@ task_xy = read.csv(file = "data/terra/task-x-y.csv")
 cache("task_xy")
 
 gpu = read.csv(file = "data/terra/gpu.csv")
-gpu$timestamp = ymd_hms(as.character(gpu$timestamp))
+gpu$timestamp = ymd_hms(gpu$timestamp)
 cache("gpu")
 
 app_check = read.csv(file = "data/terra/application-checkpoints.csv")
-app_check$timestamp = ymd_hms(as.character(app_check$timestamp))
+app_check$timestamp = ymd_hms(app_check$timestamp)
 cache("app_check")
 
 
--- src/EDA_01.R
+++ src/EDA_01.R
@@ -38,6 +38,13 @@ ggplot(gpu_t, aes(timestamp, gpuMemUtilPerc)) + geom_line()
 
 # GPU EDA - looks like GPU dataset is an automated output, taking readings at a specified time for all GPUs.
 
+# HOSTNAMES: 1024
+# TaskID: 65793 (level 12 @ 256*256; level 8 @ 16*16; level 4 @ 1*1)
+# JobID: 3 (one for each level)
+
+# GpuSerial: 1024
+# GPuUUID: 1024
+
 
 # Filter task_xy for only level 12 (because levels 4 and 8 are sparsley represented - i.e. 1 and 256 samples respectively)
 # Drop unused columns for joining
@@ -50,18 +57,39 @@ length(unique(task_xy_red$taskId))
 
 # Join task_xy_red to app_check joining on taskId
 test = left_join(app_check, task_xy_red)
+
+test = test %>%
+        mutate(jobId = NULL) %>%
+        arrange(taskId, timestamp)
+
+test2 = left_join(gpu_r, test)
     
  
 
 
 
 
+# Q1 Which tasks dominate runtimes?
+# For each different taskId, calculate difference in time for each task (stop - start) and enter in new column
+
+q1 = test %>%
+        group_by(taskId, eventName) %>%
+        summarise(duration = as.numeric(difftime(last(timestamp), first(timestamp), unit = 'sec'))) %>%
+        group_by(eventName) %>%
+        summarise(mean_dur = mean(duration))
 
 
 
+# Q2 interplay between GPU Temp and Performance
 
+q2 = gpu %>%
+        select(5:8)
 
+x = sample(1:dim(q2)[1],10000)
+q2_samp = q2[x,]
+plot(q2_samp$powerDrawWatt,q2_samp$gpuTempC)
 
+plot(q2_samp$gpuUtilPerc,q2_samp$gpuTempC)
 
 
 
